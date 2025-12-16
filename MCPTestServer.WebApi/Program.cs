04962bdd0fb3fd025d1079d010bdfbe07301dafb
Oleksandr Barabanov <obarabanov@haemonetics.com>
2025-12-16 15:01:00 +0200

    feat(Authorization): add OAUTH2 authentication

--- MCPTestServer.WebApi/Program.cs
+++ MCPTestServer.WebApi/Program.cs
@@ -1,17 +1,60 @@
-﻿using McpTestServer.Core.Extensions;
+﻿using System.Security.Claims;
+using McpTestServer.Core.Extensions;
 using MCPTestServer.WebApi.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using ModelContextProtocol.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var serverUrl = builder.Configuration["ServerUrl"] ?? throw new InvalidOperationException("ServerUrl must be defined in appSettings.");
+var authorizationServerUrl = builder.Configuration["AuthorizationServerUrl"] ?? throw new InvalidOperationException("AuthorizationServerUrl must be defined in appSettings.");
+var inspectorOrigins = builder.Configuration["InspectorOrigins"] ?? throw new InvalidOperationException("AuthorizationServerUrl must be defined in appSettings.");
+
 builder.Services.AddMcpServer()
     .WithHttpTransport()
-    .AddMcpTestServerFeatures();
-    //.AddAuthorizationFilters();
+    .AddMcpTestServerFeatures()
+    .AddAuthorizationFilters();
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultChallengeScheme = McpAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Authority = authorizationServerUrl;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+    })
+    .AddMcp(options =>
+    {
+        options.ResourceMetadata = new()
+        {
+            Resource = new Uri(serverUrl),
+            AuthorizationServers = { new Uri(authorizationServerUrl) },
+            ScopesSupported = ["mcp:tools"]
+        };
+    });
+
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("mcp-inspector", p => p
+        .WithOrigins(inspectorOrigins.Split(','))
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .WithExposedHeaders("WWW-Authenticate"));
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-app.UseRequestLogging();
-app.MapMcp(); //.RequireAuthorization();
+app.UseCors("mcp-inspector");
+app.UseAuthentication();
+app.UseAuthorization();
 
+app.UseRequestLogging();
+app.MapMcp().RequireAuthorization();
 
 await app.RunAsync("http://localhost:3001");
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # AWSMCPTestServer
+
+
+# Testing with MCP Inspector
+
+
+The Inspector runs directly through npx without requiring installation:
+
+To run HTTP version, call
+
+```bash
+npx @modelcontextprotocol/inspector http://localhost:3001
+```
+
+To run console (STDIO) version, call
+
+```bash
+npx @modelcontextprotocol/inspector dotnet run --project ./AWSMCPTestServer/AWSMCPTestServer --no-build
+```
+
