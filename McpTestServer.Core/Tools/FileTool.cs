b1727d39094b55e7b57ae18eae30a5d47a8b9069
AlBa231 <AlBa231@gmail.com>
2025-12-26 11:35:30 +0200

    feat(FileTool): add new tool to check Roots capability

---
+++ McpTestServer.Core/Tools/FileTool.cs
@@ -0,0 +1,39 @@
+﻿using ModelContextProtocol.Protocol;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace McpTestServer.Core.Tools;
+
+[McpServerToolType]
+public static class FileTool
+{
+    [McpServerTool(Title = "List the files in the roots folders.")]
+    [Description("""
+                 This tool will return a list of files.
+                 
+                 Call this tool when the user asks about listing the files.
+                 
+                 It uses roots to find boundaries. If no roots specified - returns Roots is empty message.
+                 """)]
+    public static async Task<List<string>> ListFilesByRoot(RequestContext<CallToolRequestParams> context)
+    {
+        var files = await TryGetServerRootsAsync(context.Server);
+
+        return files.Count == 0 ? ["Roots is empty"] : files;
+    }
+
+    private static async Task<List<string>> TryGetServerRootsAsync(McpServer server)
+    {
+        try
+        {
+            var roots = await server.RequestRootsAsync(new ListRootsRequestParams());
+
+            return roots.Roots.Select(r => r.Uri).ToList();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return [e.Message];
+        }
+    }
+}
