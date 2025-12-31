f90f03afb3193fc53cb150990062bdd2a4917bce
AlBa231 <AlBa231@gmail.com>
2025-12-31 15:36:31 +0200

    feat(lambda): move back to Program.cs but use another CMD for Dockerfile

---
+++ MCPTestServer.Lambda/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using McpTestServer.Core.Extensions;
+using MCPTestServer.Lambda.Extensions;
+using Microsoft.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddHttpClient()
+    .AddMcpServer()
+    .WithHttpTransport()
+    .AddMcpTestServerFeatures();
+
+builder.Services.AddHealthChecks();
+
+var appBase = builder.Configuration["BasePath"] ?? "/lambda";
+var app = builder.Build();
+app.UsePathBase(appBase);
+app.UseMcpExceptionHandling();
+
+app.Logger.Log(LogLevel.Information, "Mapping health to /health");
+app.MapHealthChecks("/health");
+
+app.MapMcp(appBase);
+
+await app.RunAsync();
