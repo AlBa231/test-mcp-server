ba9c42f31045794f321dd0512d7f17e132a89f8b
AlBa231 <AlBa231@gmail.com>
2025-12-31 14:56:41 +0200

    feat(lambda): use Startup file and LambdaEntryPoint

---
+++ MCPTestServer.Lambda/LambdaEntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MCPTestServer.Lambda;
+
+public class LambdaEntryPoint : ApplicationLoadBalancerFunction
+{
+    /// <summary>
+    /// The builder has configuration, logging and Amazon API Gateway already configured. The startup class
+    /// needs to be configured in this method using the UseStartup<>() method.
+    /// </summary>
+    /// <param name="builder">The IWebHostBuilder to configure.</param>
+    protected override void Init(IWebHostBuilder builder)
+    {
+        builder.UseStartup<Startup>();
+    }
+}
---
+++ MCPTestServer.Lambda/Startup.cs
@@ -0,0 +1,42 @@
+﻿using McpTestServer.Core.Extensions;
+using MCPTestServer.Lambda.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MCPTestServer.Lambda;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public string BasePath => Configuration["BasePath"] ?? "/labmda";
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddHttpClient()
+            .AddMcpServer()
+            .WithHttpTransport()
+            .AddMcpTestServerFeatures();
+
+        services.AddHealthChecks();
+    }
+
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UsePathBase(BasePath);
+        app.UseMcpExceptionHandling();
+
+        app.UseHealthChecks("/health");
+
+        app.UseEndpoints(builder => builder.MapMcp(BasePath));
+    }
+}
